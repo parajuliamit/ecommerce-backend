abd5cb0d4e2fd4aae2d844fc254cdd851198cae4
parajuliamit <be2017se587@gces.edu.np>
2022-02-02 17:37:17 +0545

    switched to sqlite

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b5$g4f)4o#1%-rkzpvps8jl@oj+it-ig9%#49yy_5#bn^ovbq('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','grocery-nepal.herokuapp.com','grocery-nepal.azurewebsites.net']
 # CSRF_TRUSTED_ORIGINS = AnyStr
@@ -117,23 +117,23 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER' : 'amit',
-        'PASSWORD' : os.getenv('DB_PASS'),
-        'HOST' : 'amit.postgres.database.azure.com',
-        'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER' : 'amit',
+#         'PASSWORD' : os.getenv('DB_PASS'),
+#         'HOST' : 'amit.postgres.database.azure.com',
+#         'PORT' : '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
